bfde56921d12e3956c3743dbba5b5bf9bcf0eafe
Ken Lin <kel117@pitt.edu>
2020-09-08 19:06:40 -0400

    changed route name

--- server/routes/account.js
+++ server/routes/account.js
@@ -4,6 +4,10 @@ const { Account } = require("../models/Account");
 
 const { auth } = require("../middleware/auth");
 
+//=================================
+//             Account
+//=================================
+
 router.post("/", auth, (req, res) => {
   const account = new Account({
     userId: req.body.id,
@@ -19,7 +23,7 @@ router.post("/", auth, (req, res) => {
     .catch((err) => console.log(err));
 });
 
-router.get("/all", auth, (req, res) => {
+router.get("/", auth, (req, res) => {
   Account.find({ userId: req.body.userId })
     .then((result) => {
       res.status(200).json({ success: true, accounts: result });
