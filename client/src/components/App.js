9228833ebe589fa641ea947c1c659748ab0f699c
Ken Lin <kel117@pitt.edu>
2020-09-08 18:49:48 -0400

    Added more routes

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -5,8 +5,10 @@ import Auth from "../hoc/auth";
 import LandingPage from "./views/LandingPage/LandingPage.js";
 import LoginPage from "./views/LoginPage/LoginPage.js";
 import RegisterPage from "./views/RegisterPage/RegisterPage.js";
+import AddAccountPage from "./views/AddAccountPage/AddAccountPage.js";
 import NavBar from "./views/NavBar/NavBar";
 import Footer from "./views/Footer/Footer";
+import AccountPage from "./views/AccountPage/AccountPage";
 
 //null   Anyone Can go inside
 //true   only logged in user can go inside
@@ -20,13 +22,23 @@ function App() {
         style={{
           paddingTop: "69px",
           height: "calc(100vh - 70px)",
-          backgroundColor: "#17172E",
+          backgroundColor: "#1b262c",
         }}
       >
         <Switch>
           <Route exact path="/" component={Auth(LandingPage, null)} />
           <Route exact path="/login" component={Auth(LoginPage, false)} />
           <Route exact path="/register" component={Auth(RegisterPage, false)} />
+          <Route
+            exact
+            path="/add-account"
+            component={Auth(AddAccountPage, true)}
+          />
+          <Route
+            exact
+            path="/account/:accountId"
+            component={Auth(AccountPage, true)}
+          />
         </Switch>
       </div>
       <Footer />
