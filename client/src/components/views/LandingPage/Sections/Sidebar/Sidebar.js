6b50db49a67d740620172f3464838faeae39683d
Ken Lin <kel117@pitt.edu>
2020-09-08 18:50:01 -0400

    Implemented sidebar

---
+++ client/src/components/views/LandingPage/Sections/Sidebar/Sidebar.js
@@ -0,0 +1,40 @@
+import React, { useEffect, useState } from "react";
+import Axios from "axios";
+import "./Sidebar.css";
+
+function Sidebar(props) {
+  const [accounts, setAccounts] = useState([]);
+
+  useEffect(() => {
+    Axios.get("/api/account/all").then((response) => {
+      if (response.data.success) {
+        setAccounts(response.data.accounts);
+      } else {
+        alert("Failed to get accounts");
+      }
+    });
+  }, []);
+
+  return (
+    <div className="sidebar">
+      <a href="/add-account" className="add-account-btn">
+        + New Account
+      </a>
+      {accounts.map((account, index) => {
+        return (
+          <a
+            href={`/account/${account._id}`}
+            className={`add-account-btn ${
+              props.active == account._id ? "add-account-btn-active" : ""
+            }`}
+            key={index}
+          >
+            {account.accountName}
+          </a>
+        );
+      })}
+    </div>
+  );
+}
+
+export default Sidebar;
