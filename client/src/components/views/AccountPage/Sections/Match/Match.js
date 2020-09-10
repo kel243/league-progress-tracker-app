bdd09646fe06a8a05d7271246d55b979fb47209b
Ken Lin <kel117@pitt.edu>
2020-09-10 04:37:23 -0400

    formated date object into readable format

--- client/src/components/views/AccountPage/Sections/Match/Match.js
+++ client/src/components/views/AccountPage/Sections/Match/Match.js
@@ -4,6 +4,34 @@ import "./Match.css";
 function Match(props) {
   const notesRef = useRef(null);
 
+  const months = [
+    "Jan",
+    "Feb",
+    "Mar",
+    "Apr",
+    "May",
+    "Jun",
+    "Jul",
+    "Aug",
+    "Sep",
+    "Oct",
+    "Nov",
+    "Dec",
+  ];
+
+  const date = new Date(props.date);
+
+  let formatDate = `${
+    months[date.getMonth()]
+  } ${date.getDate()}, ${date.getFullYear()} - ${
+    date.getHours() + 1 < 12
+      ? ("0" + (date.getHours() + 1)).slice("-2")
+      : ("0" + (date.getHours() - 11)).slice("-2")
+  }:${("0" + (date.getMinutes() + 1)).slice("-2")}:${(
+    "0" +
+    (date.getSeconds() + 1)
+  ).slice("-2")}${date.getHours() + 1 > 11 ? "pm" : "am"}`;
+
   const onClickHandler = () => {
     notesRef.current.classList.toggle("match-notes-expanded");
   };
@@ -43,6 +71,7 @@ function Match(props) {
         <p className={`match-text ${props.promo ? "promos" : "not-promos"}`}>
           P
         </p>
+        <p className="match-text match-text-date">{formatDate}</p>
       </div>
       <div className="match-notes" ref={notesRef}>
         <p className="match-text match-text-notes">{props.notes}</p>
