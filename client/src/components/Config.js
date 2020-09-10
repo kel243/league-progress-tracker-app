650a1a8d9003ee28a83b37e1f3675bb85a793c18
Ken Lin <kel117@pitt.edu>
2020-09-10 04:20:08 -0400

    deleted useless config settings

--- client/src/components/Config.js
+++ client/src/components/Config.js
@@ -1,8 +1,2 @@
 //SERVER ROUTES
 export const USER_SERVER = "/api/users";
-
-export const API_KEY = process.env.REACT_APP_API_KEY;
-
-export const API_URL = "https://api.themoviedb.org/3/";
-
-export const IMAGE_URL = "https://image.tmdb.org/t/p/";
