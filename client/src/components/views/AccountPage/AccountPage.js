964a03a712a3d00a4c0112f75d10c91f209c9cfe
Ken Lin <kel117@pitt.edu>
2020-10-05 18:01:55 -0400

    Added spinner

--- client/src/components/views/AccountPage/AccountPage.js
+++ client/src/components/views/AccountPage/AccountPage.js
@@ -7,11 +7,13 @@ import Match from "./Sections/Match/Match";
 import MatchForm from "./Sections/MatchForm/MatchForm";
 import AccountInfo from "./Sections/AccountInfo/AccountInfo";
 import FilterBar from "./Sections/FilterBar/FilterBar";
+import Spinner from "../Util/Spinner";
 
 function AccountPage(props) {
   const [accountFound, setAccountFound] = useState(true);
   const [matches, setMatches] = useState([]);
   const [allMatches, setAllMatches] = useState([]);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     Axios.get(`/api/account/${props.match.params.accountId}`).then(
@@ -30,6 +32,7 @@ function AccountPage(props) {
   useEffect(() => {
     Axios.get(`/api/match/${props.match.params.accountId}`).then((response) => {
       if (response.data.success) {
+        setLoading(false);
         setMatches(response.data.matches);
         setAllMatches(response.data.matches);
       }
@@ -50,63 +53,67 @@ function AccountPage(props) {
   };
 
   const content = () => {
-    if (accountFound && allMatches.length > 0) {
-      return (
-        <div className="account-main">
-          <AccountInfo
-            accountId={props.match.params.accountId}
-            matches={matches}
-          />
-          <div className="account-matches">
-            <MatchForm
+    if (loading) {
+      return <Spinner />;
+    } else {
+      if (accountFound && allMatches.length > 0) {
+        return (
+          <div className="account-main">
+            <AccountInfo
               accountId={props.match.params.accountId}
-              updateMatches={updateMatches}
-            />
-            <FilterBar
-              filterMatches={filterMatches}
               matches={matches}
-              allMatches={allMatches}
             />
-            <div className="account-matches-box">
-              {matches.map((match, index) => {
-                return (
-                  <Match
-                    accountId={props.match.params.accountId}
-                    matchId={match._id}
-                    result={match.result}
-                    champion={match.champion}
-                    opponent={match.opponent}
-                    lane={match.lane}
-                    lp={match.lpChange}
-                    promo={match.promo}
-                    notes={match.notes}
-                    date={match.date}
-                    updateMatches={updateMatches}
-                    key={`match-${index}`}
-                  />
-                );
-              })}
+            <div className="account-matches">
+              <MatchForm
+                accountId={props.match.params.accountId}
+                updateMatches={updateMatches}
+              />
+              <FilterBar
+                filterMatches={filterMatches}
+                matches={matches}
+                allMatches={allMatches}
+              />
+              <div className="account-matches-box">
+                {matches.map((match, index) => {
+                  return (
+                    <Match
+                      accountId={props.match.params.accountId}
+                      matchId={match._id}
+                      result={match.result}
+                      champion={match.champion}
+                      opponent={match.opponent}
+                      lane={match.lane}
+                      lp={match.lpChange}
+                      promo={match.promo}
+                      notes={match.notes}
+                      date={match.date}
+                      updateMatches={updateMatches}
+                      key={`match-${index}`}
+                    />
+                  );
+                })}
+              </div>
             </div>
           </div>
-        </div>
-      );
-    } else if (accountFound) {
-      return (
-        <div className="account-main">
-          <AccountInfo accountId={props.match.params.accountId} />
-          <div className="account-matches">
-            <MatchForm
-              accountId={props.match.params.accountId}
-              updateMatches={updateMatches}
-            />
-            <div className="account-matches-box">
-              <h1 className="landing-heading">No matches found!</h1>;
+        );
+      } else if (accountFound) {
+        return (
+          <div className="account-main">
+            <AccountInfo accountId={props.match.params.accountId} />
+            <div className="account-matches">
+              <MatchForm
+                accountId={props.match.params.accountId}
+                updateMatches={updateMatches}
+              />
+              <div className="account-matches-box">
+                <h1 className="landing-heading">No matches found!</h1>;
+              </div>
             </div>
           </div>
-        </div>
-      );
-    } else if (!accountFound) {
-      return <h1 className="landing-heading">Account not found!</h1>;
+        );
+      } else if (!accountFound) {
+        return <h1 className="landing-heading">Account not found!</h1>;
+      }
     }
   };
 
